e8c4e9086413dec46b8b73ef2e1de8568aea7f95
ankoud <hamdi.ankoud@gmail.com>
2016-10-07 13:22:38 +0100

    mail issue  fixed

--- app/src/main/java/com/example/ankoud/bookforyou/ProfileActivity.java
+++ app/src/main/java/com/example/ankoud/bookforyou/ProfileActivity.java
@@ -51,14 +51,14 @@ public class ProfileActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout) ;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this) ;
         View header =navigationView.getHeaderView(0);
         TextView emailTxt = (TextView)header.findViewById(R.id.textuseremail);
         emailTxt.setText(firebaseAuth.getCurrentUser().getEmail());
